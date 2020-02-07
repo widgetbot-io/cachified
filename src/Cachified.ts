59be99b4a39fef4fd4040af066619a1caea4bb42
viction <viction.dev@gmail.com>
2020-02-07 18:06:07 +0000

    Add support for JSON parsable values

--- src/Cachified.ts
+++ src/Cachified.ts
@@ -1,6 +1,7 @@
 import {GetConfig} from "./configure";
 import {CachifiedInstance} from "./types";
 
+
 export function Cachified(expirySeconds: number = 24 * 60 * 60): MethodDecorator { // One day expiry default
     return function(target: any, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<any>) {
         const oldFunc = descriptor.value;
@@ -13,7 +14,23 @@ export function Cachified(expirySeconds: number = 24 * 60 * 60): MethodDecorator
             const redisKey = `cachified:${oldFunc.name}_${args.join('-')}`;
 
             const val = await getRawVal(config, redisKey);
-            console.log(val);
+            if (val) {
+                try {
+                    return JSON.parse(val);
+                } catch {
+                    return val;
+                }
+            }
+
+            const returnedData = await oldFunc.apply(this, args);
+
+            try {
+                await setRawValWithExpiry(config, redisKey, JSON.stringify(returnedData), expirySeconds);
+            } catch {
+                await setRawValWithExpiry(config, redisKey, returnedData, expirySeconds);
+            }
+
+            return returnedData;
         }
     }
 }
@@ -22,4 +39,10 @@ function getRawVal(config: CachifiedInstance, key: string) {
     if (!config.client) throw new Error('Attempted Cachified usage without initialization');
 
     return config.client.get(key);
+}
+
+function setRawValWithExpiry(config: CachifiedInstance, key: string, value: any, expiry: number) {
+    if (!config.client) throw new Error('Attempted Cachified usage without initialization');
+
+    return config.client.set(key, value, 'EX', expiry);
 }
